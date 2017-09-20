f090b028a7bf8bae37f54cff49969180f8203476
yuchuang <yuchuang0208@163.com>
2017-09-20 14:39:12 +0800

    模板引擎完成登录

--- public/js/common.js
+++ public/js/common.js
@@ -1,4 +1,4 @@
-define(["jquery","cookie"], function ($) {
+define(["jquery","template","cookie"], function ($,template) {
 	//NProgress.start();
 	//NProgress.done();
 
@@ -34,8 +34,17 @@ define(["jquery","cookie"], function ($) {
 	var loginInfo = $.cookie("loginInfo");
 	//如果登录信息存在转化成数组
 	loginInfo = loginInfo && JSON.parse(loginInfo);
-	$(".aside .profile img").attr("src",loginInfo.tc_avatar);
-	$(".aside .profile h4").html(loginInfo.tc_name);
+	//$(".aside .profile img").attr("src",loginInfo.tc_avatar);
+	//$(".aside .profile h4").html(loginInfo.tc_name);
+	var tpl = '<div class="avatar img-circle">' +
+		' <img src="{{tc_avatar}}">' +
+		' </div>' +
+		' <h4>{{tc_name}}</h4>';
+	var html = template.render(tpl,loginInfo);
+	$(".aside .profile").html(html);
+
+
+	
 
 
 });
--- public/js/config.js
+++ public/js/config.js
@@ -5,7 +5,9 @@ require.config({
     paths: {
         jquery: "jquery/jquery",
         cookie: "jquery-cookie/jquery.cookie",
+        template: "artTemplate/template-web",
         common: "../js/common",
         login: "../js/login"
+
     }
 });
